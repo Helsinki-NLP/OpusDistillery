664a122f82b4d765498209a118e42cc973e18367
Ona de Gibert <odegiber@gmail.com>
2026-02-03 10:16:12 +0200

    fixed ctranslate scripts

--- pipeline/translate/translate_ctranslate.py
+++ pipeline/translate/translate_ctranslate.py
@@ -1,6 +1,6 @@
 import os
-#os.environ["CTRANSLATE2_LOG_LEVEL"] = "DEBUG"
-#os.environ["CT2_VERBOSE"] = "3"
+os.environ["CTRANSLATE2_LOG_LEVEL"] = "DEBUG"
+os.environ["CT2_VERBOSE"] = "3"
 import ctranslate2
 from transformers import AutoTokenizer
 import time
@@ -39,15 +39,16 @@ def split_list_into_sublists(input_list, sublist_size=1):
 
 # Define a function to translate a batch using a specific translator
 def translate_batch(batch, translator, tokenizer, src_lang, tgt_lang, model_name, batch_size=64):
+    num_return_sequences = 8
     if "indictrans" in model_name:
         ip = IndicProcessor(inference=True)
         batch = ip.preprocess_batch(batch, src_lang=src_lang, tgt_lang=tgt_lang)
-    tokenized_batch = [tokenizer.convert_ids_to_tokens(tokenizer.encode(text)) for text in batch]
-    if 'nllb' or 'indic' in model_name:
+    tokenized_batch = [tokenizer.convert_ids_to_tokens(tokenizer.encode(text[:256])) for text in batch]
+    if 'nllb' in model_name:
         tgt_prefix_list = [[tgt_lang]] * len(tokenized_batch)
-        results = translator.translate_batch(tokenized_batch, target_prefix=tgt_prefix_list, beam_size=8, num_hypotheses=8, batch_type="tokens", max_batch_size=batch_size)
+        results = translator.translate_batch(tokenized_batch, target_prefix=tgt_prefix_list, beam_size=num_return_sequences, num_hypotheses=num_return_sequences, batch_type="tokens", max_batch_size=batch_size)
     else:
-        results = translator.translate_batch(tokenized_batch, beam_size=8, num_hypotheses=8, batch_type="tokens", max_batch_size=batch_size)
+        results = translator.translate_batch(tokenized_batch, beam_size=num_return_sequences, num_hypotheses=num_return_sequences, batch_type="tokens", max_batch_size=batch_size)
     hypotheses = []
     for result in results:
         for hyp in result.hypotheses:
@@ -63,24 +64,33 @@ def translate_batch(batch, translator, tokenizer, src_lang, tgt_lang, model_name
 
 # Worker function to process batches from the task queue
 def translate_worker(translator, task_queue, output_lock, output_file, tokenizer, src_lang, tgt_lang, model_name, logfile, batch_size=64, progress_bar=None):
+    print(f"Worker started on device {translator.device_index}", flush=True)
+    num_return_sequences = 8
     while not task_queue.empty():
         try:
+            # Log the current size of the task queue
+            print(f"Task queue size: {task_queue.qsize()}", file=logfile, flush=True)
+            
             batch = task_queue.get()  # Get the batch (index, sentence) tuples
             indices, sentences = zip(*batch)  # Separate the indices and the sentences
 
+            # Augment sentence indices for writing
+            augmented_sentence_ids = [x for x in indices for _ in range(num_return_sequences)]
             translated_batches = translate_batch(sentences, translator, tokenizer, src_lang, tgt_lang, model_name, batch_size=batch_size)
-            translated_batches = split_list_into_sublists(translated_batches)
-            
+
+            # Synchronize GPU operations for the current device
+            torch.cuda.synchronize(translator.device_index)
+
             # Write the results to the target file
             with output_lock:
                 with open(output_file, 'a', encoding='utf-8') as tgt:
-                    for index, translations in zip(indices, translated_batches):
-                        for translation in translations:
-                            tgt.write(f"{index} ||| {translation}\n")
+                    for index, translation in zip(augmented_sentence_ids, translated_batches):
+                        tgt.write(f"{index} ||| {translation}\n")
             
             if progress_bar:
                 progress_bar.update(len(batch))
         except queue.Empty:
+            torch.cuda.synchronize(translator.device_index)  # Ensure GPU synchronization before exiting
             break
 
 # Main translation function with GPU or CPU distribution and tracking
@@ -107,8 +117,8 @@ def translate_file(source_file, target_file, translators, num_devices, tokenizer
     with tqdm(total=total_batches, desc="Translating", unit=" lines") as progress_bar:
         # Using ThreadPoolExecutor to manage parallel processing
         with ThreadPoolExecutor(max_workers=num_devices) as executor:
-            for device_index in range(num_devices):
-                executor.submit(translate_worker, translators[device_index], task_queue, output_lock, target_file, tokenizer, src_lang, tgt_lang, model_name, logfile, batch_size, progress_bar)
+            for device_index, translator in enumerate(translators):
+                executor.submit(translate_worker, translator, task_queue, output_lock, target_file, tokenizer, src_lang, tgt_lang, model_name, logfile, batch_size, progress_bar)
 
     end_time = time.time()
     print(f"Translation completed in {end_time - start_time:.2f} seconds.", file=logfile, flush=True)
@@ -146,15 +156,20 @@ def main():
     # Initialize a tokenizer
     if args.langinfo in ["True", "true", "1"]:
         print(f"Loading tokenizer with language tags: {src_lang} and {tgt_lang}", file=logfile, flush=True)
-        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, src_lang=src_lang, tgt_lang=tgt_lang, use_fast=True)
+        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, max_length=256, truncation=True, padding="longest", src_lang=src_lang, tgt_lang=tgt_lang, use_fast=True)
     else:
-        tokenizer = AutoTokenizer.from_pretrained(model_name, truncation=True, padding="longest", trust_remote_code=True, use_fast=True)
+        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, max_length=256, truncation=True, padding="longest", use_fast=True)
     print(f"Tokenizer loaded!", file=logfile, flush=True)
 
     
     print("Loading model...", file=logfile, flush=True)
     # Initialize a translator instance for each device (either GPU or CPU)
-    translators = [ctranslate2.Translator(model_path, device=device_type, device_index=i if device_type == "cuda" else 0) for i in range(num_devices)]
+    #translators = [ctranslate2.Translator(model_path, device=device_type, device_index=i if device_type == "cuda" else 0) for i in range(num_devices)]
+    translators = [
+        ctranslate2.Translator(
+            model_path, device=device_type, device_index=i
+        ) for i in range(num_devices)
+    ]
 
     print("Model loaded!", file=logfile, flush=True)
 
@@ -167,6 +182,9 @@ def main():
     target_file = args.fileout
     translate_file(source_file, target_file, translators, num_devices, tokenizer, logfile, src_lang, tgt_lang, model_name, batch_size=batch_size)
 
+    for device_index in range(num_devices):
+        torch.cuda.synchronize(device_index)
+        
     print(f"Translation completed. Results are saved in '{target_file}'", file=logfile, flush=True)
 
 if __name__ == "__main__":
--- pipeline/translate/translate_ctranslate.sh
+++ pipeline/translate/translate_ctranslate.sh
@@ -55,6 +55,8 @@ fi
 
 echo "### Translation started"
 
+nvidia-smi --query-gpu=timestamp,name,pci.bus_id,driver_version,pstate,pcie.link.gen.max,pcie.link.gen.current,temperature.gpu,utilization.gpu,utilization.memory,power.draw,memory.total,memory.free,memory.used --format=csv -l 1 > $logfile.gpu &
+
 python pipeline/translate/translate_ctranslate.py $filein $fileout.tmp $modelname $modeldir $src_lang $trg_lang $langinfo $prompt "$langtags" $config $batch_size $logfile
 
 echo "### Done!"
