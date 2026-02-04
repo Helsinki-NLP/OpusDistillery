50f49d8399ad0be369cf63c7dfd58052785c3d8a
onadegibert <odegiber@gmail.com>
2026-02-04 11:20:36 +0200

    fixed files for containerization

--- pipeline/setup/install-deps.sh
+++ pipeline/setup/install-deps.sh
@@ -19,11 +19,11 @@ apt-get install -y libhunspell-dev
 echo "### Installing marian dependencies"
 apt-get install -y git cmake build-essential libboost-system-dev libprotobuf17 protobuf-compiler libprotobuf-dev openssl libssl-dev libgoogle-perftools-dev
 
-echo "### Installing Intel MKL"
-wget -qO- 'https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB' | apt-key add -
-sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
-apt-get update
-apt-get install -y intel-mkl-64bit-2020.0-088
+# echo "### Installing Intel MKL"
+# wget -qO- 'https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB' | apt-key add -
+# sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
+# apt-get update
+# apt-get install -y intel-mkl-64bit-2020.0-088
 
 echo "### Installing fast_align dependencies "
 apt-get install -y libgoogle-perftools-dev libsparsehash-dev libboost-all-dev
